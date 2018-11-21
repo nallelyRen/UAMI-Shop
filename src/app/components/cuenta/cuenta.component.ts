c60528b3da40c1f339c4c2732a984d1f5a3ed566
nallelyRen <nallelykagamine@gmail.com>
2018-11-21 16:54:48 -0600

    union con back

--- src/app/components/cuenta/cuenta.component.ts
+++ src/app/components/cuenta/cuenta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ProductosService } from '../../services/productos.service';
 
 @Component({
   selector: 'app-cuenta',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CuentaComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  productos: any[] = [];
+  categoria = 'Libros'; 
+  constructor(private productoService: ProductosService) {
   }
 
+  ngOnInit() {
+    this.obtenerProductos();
+  }
+
+  cambio(categoria) {
+    this.categoria = categoria;
+    if (categoria === 'libros'){
+      this.productoService.obtenerLibros().subscribe(res => {
+        this.productos = res;
+        console.log(res);
+        console.log('tipo', typeof(this.productos));
+      });
+    } else {
+      if (categoria === 'proyectos'){
+        this.productoService.obtenerProyectos().subscribe(res => {
+          this.productos = res;
+          console.log(res);
+          console.log('tipo', typeof(this.productos));
+        });
+      } else {
+      this.productoService.obtenerElectronicos().subscribe(res => {
+        this.productos = res;
+        console.log(res);
+        console.log('tipo', typeof(this.productos));
+      });
+    }
+    }
+  }
+
+  obtenerProductos() {
+    this.productoService.obtenerProductos().subscribe(res => {
+      this.productos = res;
+      console.log(res);
+      console.log('tipo', typeof(this.productos));
+    });
+}
 }
