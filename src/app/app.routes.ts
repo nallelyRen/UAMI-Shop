6af003036151c44eaf4be09bfdb90e0077992cce
jorge eduardo valencia loyola <jevl290397@gmail.com>
2019-02-13 22:52:30 -0600

    recargar la pagina resuelto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -28,4 +28,4 @@ const APP_ROUTES: Routes = [
     {path: '**', pathMatch: 'full', redirectTo: 'principal'}
 ];
 
-export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, {useHash: true});
