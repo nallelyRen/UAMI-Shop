2a3aec0c3ca5090a1c12f1d1a12b4ca88fc6f112
nallelyRen <nallelykagamine@gmail.com>
2019-03-20 16:37:57 -0600

    snackbar y console log de modificar datos de producto

--- src/app/components/modificar-datos-producto/modificar-datos-producto.component.ts
+++ src/app/components/modificar-datos-producto/modificar-datos-producto.component.ts
@@ -3,6 +3,8 @@ import { ProductosService } from '../../services/productos.service';
 import { UsuarioService } from '../../services/usuario.service';
 import { ActivatedRoute } from '@angular/router';
 import {Router} from '@angular/router';
+import { SnackbarService } from '../../services/snackbar.service';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
  selector: 'app-modificar-datos-producto',
@@ -26,12 +28,12 @@ export class ModificarDatosProductoComponent implements OnInit {
  producto: any;
 
  constructor(private usuarioService: UsuarioService, private productoService: ProductosService,
-             private router: ActivatedRoute, private router1: Router) {
+             private router: ActivatedRoute, private router1: Router,public snackbarService: SnackbarService, private snackBar: MatSnackBar) {
               this.router.params.subscribe(params => {
-                console.log(params['id']);
+               // console.log(params['id']);
                 this.id = params['id'];
                 this.producto = this.productoService.getProducto();
-                 console.log(this.producto);
+                // console.log(this.producto);
                  if (this.producto == null) {
                       this.producto = this.productoService.obtenerProductoConId(this.id).subscribe(res => {
                       this.producto = res;
@@ -57,21 +59,21 @@ validarPrecio(precio: number) {
 }
 
  guardarCambios() {
-  console.log((<HTMLInputElement>document.getElementById('precio')).value);
+ // console.log((<HTMLInputElement>document.getElementById('precio')).value);
   this.carga = true;
   this.elemento.nombre = (<HTMLInputElement>document.getElementById('nombre')).value;
   this.elemento.precio = (<HTMLInputElement>document.getElementById('precio')).value;
   this.elemento.descripcion = (<HTMLInputElement>document.getElementById('descripcion')).value;
   if (this.elemento.nombre.length < 1 || this.elemento.precio === null || this.validarPrecio(parseFloat( this.elemento.precio))
      || this.elemento.descripcion.length < 1) {
-    alert('Los campos no pueden ser vacíos y el precio debe ser menor de 10000, complete la información para continuar');
+     this.snackbarService.openmore("Los campos no pueden ser vacíos y el precio debe ser menor de 10000, complete la información para continuar","");
     this.carga = false;
   } else {
    if (this.producto.representante) {
     this.elemento.representante = (<HTMLInputElement>document.getElementById('representante')).value;
     this.elemento.requisitos = (<HTMLInputElement>document.getElementById('requisitos')).value;
     if (this.elemento.representante.length < 1 || this.elemento.requisitos.length < 1) {
-      alert('Los campos no pueden ser vacíos, complete la información para continuar');
+      this.snackbarService.open("Los campos no pueden ser vacíos, complete la información para continuar","");
       this.carga = false;
     } else {
       const id= this.usuarioService.validarUsuarios();    
@@ -79,11 +81,11 @@ validarPrecio(precio: number) {
     this.productoService.modificaDatosProyectos(id, this.producto.id, this.elemento.nombre, this.elemento.precio,
       this.elemento.descripcion, this.elemento.representante, this.elemento.requisitos).subscribe(res => {
         if (res) {
-          alert('Tu proyecto "' + this.elemento.nombre  + '" se ha modificado correctamente, los cambios pueden demorar unos minutos en aparecer');
-          this.carga = false;
+          this.snackbarService.openmore(this.elemento.nombre  +" se ha modificado correctamente, los cambios pueden demorar unos minutos en aparecer","");
+           this.carga = false;
           this.producto.nombre = this.elemento.nombre;
         } else {
-          alert('Ups, tu proyecto "' + this.producto.nombre + '" no se ha podido modificar');
+          this.snackbarService.open(this.producto.nombre +" no se ha podido modificar","");          
           this.carga = false;
         }
       });
@@ -93,8 +95,8 @@ validarPrecio(precio: number) {
    } else {
     if (this.producto.ubicacion) {
       this.elemento.ubicacion = (<HTMLInputElement>document.getElementById('ubicacion')).value;
-      if (this.elemento.ubicacion.length < 1 ) {
-        alert('Los campos no pueden ser vacíos, complete la información para continuar');
+      if (this.elemento.ubicacion.length < 1 ) {        
+        this.snackbarService.open("Los campos no pueden ser vacíos, complete la información para continuar","");
         this.carga = false;
       } else {
         const id= this.usuarioService.validarUsuarios();
@@ -102,11 +104,11 @@ validarPrecio(precio: number) {
       this.productoService.modificaDatosDepartamentos(id, this.producto.id, this.elemento.nombre, this.elemento.precio,
         this.elemento.descripcion, this.elemento.ubicacion).subscribe(res => {
           if (res) {
-            alert('Tu departamento "' + this.elemento.nombre  + '" se ha modificado correctamente, los cambios pueden demorar unos minutos en aparecer');
+            this.snackbarService.openmore(this.elemento.nombre  +" se ha modificado correctamente, los cambios pueden demorar unos minutos en aparecer","");
             this.carga = false;
             this.producto.nombre = this.elemento.nombre;
           } else {
-            alert('Ups, tu depatramento "' + this.producto.nombre + '" no se ha podido modificar');
+            this.snackbarService.open(this.producto.nombre +" no se ha podido modificar","");
             this.carga = false;
           }
         });
@@ -116,7 +118,7 @@ validarPrecio(precio: number) {
       if (this.producto.area) {
         this.elemento.area = (<HTMLInputElement>document.getElementById('area')).value;
         if (this.elemento.area.length < 1) {
-          alert('Los campos no pueden ser vacíos, complete la información para continuar');
+          this.snackbarService.open("Los campos no pueden ser vacíos, complete la información para continuar","");
           this.carga = false;
         } else {
           const id= this.usuarioService.validarUsuarios();    
@@ -124,11 +126,11 @@ validarPrecio(precio: number) {
         this.productoService.modificaDatosTutoria(id, this.producto.id, this.elemento.nombre, this.elemento.precio,
           this.elemento.descripcion, this.elemento.area).subscribe(res => {
             if (res) {
-              alert('La tutoría "' + this.elemento.nombre  + '" se ha modificado correctamente, los cambios pueden demorar unos minutos en aparecer');
+              this.snackbarService.openmore(this.elemento.nombre  +" se ha modificado correctamente, los cambios pueden demorar unos minutos en aparecer","");
               this.carga = false;
               this.producto.nombre = this.elemento.nombre;
             } else {
-              alert('Ups, la tutoría "' + this.producto.nombre + '" no se ha podido modificar');
+              this.snackbarService.open(this.producto.nombre +" no se ha podido modificar","");
               this.carga = false;
             }
           });
@@ -140,11 +142,11 @@ validarPrecio(precio: number) {
         this.productoService.modificaDatosProducto(id, this.producto.id, this.elemento.nombre, this.elemento.precio,
            this.elemento.descripcion).subscribe(res => {
             if (res) {
-              alert('El producto "' + this.elemento.nombre + '" se ha modificado correctamente, los cambios pueden demorar unos minutos en aparecer');
+              this.snackbarService.openmore(this.elemento.nombre  +" se ha modificado correctamente, los cambios pueden demorar unos minutos en aparecer","");
               this.carga = false;
               this.producto.nombre = this.elemento.nombre;
             } else {
-              alert('Ups, tu producto "' + this.producto.nombre + '" no se ha podido modificar');
+              this.snackbarService.open(this.producto.nombre +" no se ha podido modificar","");
               this.carga = false;
             }
            });
