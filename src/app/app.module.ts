27c6ba2380088b238986d010c6e084f7cf39d643
nallelyRen <nallelykagamine@gmail.com>
2019-03-19 19:22:28 -0600

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatPaginatorModule,} from '@angular/material/paginator';
 import { MatInputModule, } from '@angular/material';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -62,7 +63,7 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     ReactiveFormsModule,
     BrowserAnimationsModule,
     MatProgressSpinnerModule,
-    MatPaginatorModule,
+    NgbPaginationModule,
     NgbModule,
     MatInputModule,
     MatSnackBarModule,
